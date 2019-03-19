e6437f546519b5c99232d947262cc71664e59da5
meixg <meixg@foxmail.com>
2019-03-19 23:43:09 +0800

    feat: add test

---
+++ test/multiply.test.js
@@ -0,0 +1,20 @@
+/**
+ * @file test
+ * @author meixuguang
+ */
+
+const assert = require('assert');
+const {VerticalCalculator} = require('../dist/index.js');
+
+const cal = new VerticalCalculator();
+
+
+
+describe('Multiply', function () {
+    it('25 * 16', function () {
+        const res = cal.generate(25, '*', 16);
+        const result = [{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"2"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"5"}]}]},{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"X"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"1"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"6"}]}]},{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"1"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"5"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"0"}]}]},{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"2"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"5"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":""}]}]},{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"4"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"0"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"0"}]}]}]; // eslint-disable-line
+        assert.equal(res.code, 0);
+        assert.deepEqual(res.result, result);
+    });
+});
