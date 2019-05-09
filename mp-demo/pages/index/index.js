95f1ea22dbc1dd94ee1e75e1e48679baa523f88e
meixg <meixg@foxmail.com>
2019-05-09 11:40:29 +0800

    feat: 完善 demo

--- mp-demo/pages/index/index.js
+++ mp-demo/pages/index/index.js
@@ -2,42 +2,40 @@
  * @file index.js
  * @author swan
  */
+
+import {VerticalCalculator} from '../../../dist';
+
 const app = getApp()
+const cal = new VerticalCalculator({
+    tdClass: '',
+    trClass: ''
+});
 
 Page({
-  data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: swan.canIUse('button.open-type.getUserInfo'),
-    nodes: [{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":"."}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":"."}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":"."}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;color: red;"},"children":[{"type":"text","text":""}]}]},{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"1"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"2"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"2"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"2"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"."}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"9"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"9"}]}]},{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"-"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"9"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"9"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"9"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"."}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"0"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;border-bottom-color: #000;width: 10px;"},"children":[{"type":"text","text":"0"}]}]},{"name":"tr","attrs":{"class":""},"children":[{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":""}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"2"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"2"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"3"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"."}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"9"}]},{"name":"td","attrs":{"class":"","style":"border: 1px solid transparent;width: 10px;"},"children":[{"type":"text","text":"9"}]}]}]
-  },
-  onLoad() {
-    
-  },
-  getUserInfo(e) {
-    swan.login({
-      success: () => {
-        swan.getUserInfo({
-            success:(res)=> {
-                this.setData({
-                  userInfo: res.userInfo,
-                  hasUserInfo: true
-                });
-            },
-            fail: ()=> {
-              this.setData({
-                userInfo: e.detail.userInfo,
-                hasUserInfo: true
-              });
-            }
-        });
-      },
-      fail: () => {
-        swan.showModal({
-          title: '未登录',
-          showCancel: false
-        });
-      }
-    });
-  }
+    data: {
+        numa: 123.456,
+        numb: 456,
+        operator: '*',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: swan.canIUse('button.open-type.getUserInfo'),
+        nodes: [{ "name": "tr", "attrs": { "class": "" }, "children": [{ "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "." }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "." }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "." }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;color: red;" }, "children": [{ "type": "text", "text": "" }] }] }, { "name": "tr", "attrs": { "class": "" }, "children": [{ "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "1" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "2" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "2" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "2" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "." }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "9" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "9" }] }] }, { "name": "tr", "attrs": { "class": "" }, "children": [{ "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "-" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "9" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "9" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "9" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "." }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "0" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;border-bottom-color: #000;width: 10px;" }, "children": [{ "type": "text", "text": "0" }] }] }, { "name": "tr", "attrs": { "class": "" }, "children": [{ "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "2" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "2" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "3" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "." }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "9" }] }, { "name": "td", "attrs": { "class": "", "style": "border: 1px solid transparent;width: 10px;" }, "children": [{ "type": "text", "text": "9" }] }] }]
+    },
+    onLoad() {
+        global.aaa = this;
+    },
+    tapHandler() {
+        const res = cal.generate(
+            +this.getData('numa'),
+            this.getData('operator'),
+            +this.getData('numb')
+        );
+        console.log('result', res);
+        this.setData('nodes', res.nodes);
+    },
+    changeHandler(val) {
+        console.log(val);
+        const name = val.target.dataset.name;
+        this.setData(name, val.detail.value);
+    }
 })
